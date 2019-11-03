140145e078ec8ccb20af54fb251120aad75465de
dwb217 <davidwbender@gmail.com>
2019-11-03 16:34:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 ######### Import your libraries #######
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output, State
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
