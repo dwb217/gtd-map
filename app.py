27894e8b0c551e8de42555367219c72bcb91f3c2
dwb217 <davidwbender@gmail.com>
2019-11-05 22:02:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,12 @@ import dash_table
 #import gtd data
 df = pd.read_csv('gtd.csv')
 total_attacks = df['eventid'].value_counts()
-group_name = df['group']
 group_list = list(df['group'].value_counts().sort_index().index)
 country_list = list(df['country'].value_counts().sort_index().index)
+
+df['newdate'] = pd.to_datetime(df['date'])
+df['year'] = df['newdate'].dt.year
+
 
 # Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -28,15 +31,26 @@ app.title='Terrorism'
 app.layout = html.Div(children=[
     html.H1('Terrorist Attacks: 2002-2018'),
     dcc.Dropdown(
-        id='dropdown',
+        id='dropdown_groups',
         options=[{'label': i, 'value': i} for i in group_list],
         value=group_list[0]
     ),
     dcc.Graph(id='group-display'),
     html.Br(),
-    html.H3('Filter by country'),
+    html.H3('Filter by year'),
+    dcc.Slider(
+        id='slider',
+        min=2002,
+        max=2018,
+        step=1,
+        marks={i:str(i) for i in range(2002,2019)},
+        value=2002
+    ),
+    html.Br(),
+    dcc.Graph(id='year-display'),
+    html.Br(),
     dcc.Dropdown(
-        id='dropdown1',
+        id='dropdown_countries',
         options=[{'label': i, 'value': i} for i in country_list],
         value=country_list[0]
     ),
@@ -51,7 +65,7 @@ app.layout = html.Div(children=[
 ### app callback #1
 
 @app.callback(dash.dependencies.Output('group-display', 'figure'),
-              [dash.dependencies.Input('dropdown', 'value')])
+              [dash.dependencies.Input('dropdown_groups', 'value')])
 def group_picker(group_id):
     group_df=df[df['group']==group_id]
     fig = go.Figure(go.Densitymapbox(lat=group_df['latitude'], lon=group_df['longitude'], z=total_attacks, radius=5))
@@ -62,6 +76,33 @@ def group_picker(group_id):
                  )
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return fig
+
+
+
+@app.callback(dash.dependencies.Output('year-display', 'figure'),
+              [dash.dependencies.Input('slider', 'value')])
+def year_picker(year_id):
+    year_df=df[df['year']==year_id]
+    fig1 = go.Figure(go.Densitymapbox(lat=year_df['latitude'], lon=year_df['longitude'], z=total_attacks, radius=5))
+    fig1.update_layout(mapbox_style="stamen-terrain",
+                  mapbox_center_lon=0,
+                  mapbox_center_lat=0,
+                  mapbox_zoom=1,
+                 )
+    fig1.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig1
+
+
+@app.callback(dash.dependencies.Output('country-display', 'figure'),
+              [dash.dependencies.Input('dropdown_countries', 'value')])
+def country_picker(country_id):
+    country_id=df[df['country']==country_id]
+    fig2 = go.Scatter(
+            x='year',
+            y='total_attacks',
+            mode='lines'
+            )
+    return fig2
 
 # ### app callback #1
 # @app.callback(dash.dependencies.Output('country-display', 'figure'),
