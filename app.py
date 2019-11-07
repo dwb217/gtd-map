fea8d931bb982069c84ce42df9489df764cb7f8e
dwb217 <davidwbender@gmail.com>
2019-11-07 10:40:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app.title='Terrorism'
 
 app.layout = html.Div(children=[
     html.H1('Terrorist Attacks: 2002-2018'),
-    html.Br(),
+#     html.Br(),
 #     html.H3('This dataset comes from Global Terrorism Database, hosted at University of Maryland.'),
 #     html.H3('It identifies 116,912 attacks since 2002.'),
 #     html.Br(),
